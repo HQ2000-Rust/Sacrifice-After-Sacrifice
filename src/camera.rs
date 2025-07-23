261d8c20612ef8c31b785b15f773c2a1a4dd95e2
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-07-23 07:27:34 +0200

    Create camera.rs
    prob unused, uploading anyway

---
+++ src/camera.rs
@@ -0,0 +1,17 @@
+use bevy::prelude::*;
+
+pub enum CameraType {
+    Menu,
+    Level,
+}
+
+/*pub fn camera(camera_type: CameraType) -> Camera {
+    match camera_type {
+        CameraType::Menu => {
+            Camera2d:
+        }
+        CameraType::Level => {
+
+        }
+    }
+}*/
