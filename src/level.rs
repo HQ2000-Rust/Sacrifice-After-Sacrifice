f99952081166a3c3fbe0dfdb53f71b5bf7eed07f
Jonathan Schmittat <194770151+HQ2000-Rust@users.noreply.github.com>
2025-07-23 07:25:06 +0200

    Update level.rs

--- src/level.rs
+++ src/level.rs
@@ -1,4 +1,33 @@
-#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug, Ord, PartialOrd)]
+use crate::{GameState, level};
+use bevy::prelude::*;
+
+pub struct LevelPlugin;
+
+impl Plugin for LevelPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(GameState::Level), setup);
+        app.add_systems(Update, level_loop.run_if(is_in_level));
+        app.init_state::<Level>();
+        app.init_state::<Paused>();
+    }
+}
+
+#[derive(Default, Clone, Copy, PartialEq, Eq, Hash, Debug, Ord, PartialOrd, States)]
+#[states(scoped_entities)]
 pub enum Level {
+    #[default]
+    None,
     L1,
 }
+
+#[derive(Default, Clone, Copy, PartialEq, Eq, Hash, Debug, Ord, PartialOrd, States)]
+#[states(scoped_entities)]
+pub struct Paused(bool);
+
+pub fn setup(mut commands: Commands) {}
+
+pub fn is_in_level(game_state: Res<State<GameState>>) -> bool {
+    game_state.get() == &GameState::Level
+}
+
+fn level_loop() {}
