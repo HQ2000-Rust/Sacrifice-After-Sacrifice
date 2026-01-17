bd544c5ab1e76a2e8645e8999a9503988dbb21ec
aal337 <abdurahmanalgheb@gmail.com>
2026-01-17 21:52:00 +0100

    make it compile

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,14 @@ fn main() -> AppExit {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins((ScreensPlugin))
-        .add_systems(detect_screen_change)
+        .add_systems(Update, detect_screen_change)
         .run()
 }
 
 fn detect_screen_change(now: Res<State<Screen>>, next: Res<NextState<Screen>>) {
     match *next {
-        NextState::Pending()
-        NextState::Unchanged => {}
+        NextState::Pending(_) => (),
+        NextState::Unchanged => {},
+        _ => ()
     }
 }
