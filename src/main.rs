48f4bf9ca84831559b5c355ad340ce57fe3f023c
HQ2000-CPP <194770151+HQ2000-CPP@users.noreply.github.com>
2026-01-19 15:44:32 +0100

    refactor to make it compile (temp)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod asset_management;
 mod screens;
 
-use bevy::prelude::*;
+use bevy::{input::common_conditions::input_just_pressed, prelude::*};
 
 use crate::screens::{Screen, ScreensPlugin};
 
@@ -9,13 +9,21 @@ fn main() -> AppExit {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugins((ScreensPlugin))
-        .add_systems(detect_screen_change)
+        .add_systems(PreUpdate,detect_screen_change)
+        .add_systems(Update, (|mut state:  ResMut<NextState<Screen>>| {
+            state.set(Screen::TitleScreen);
+            info!("manual change to title screen via space key")
+            }).run_if(input_just_pressed(KeyCode::Space)))
         .run()
 }
 
-fn detect_screen_change(now: Res<State<Screen>>, next: Res<NextState<Screen>>) {
-    match *next {
-        NextState::Pending()
-        NextState::Unchanged => {}
-    }
+fn detect_screen_change(mut last: Local<Option<Screen>>,now: Res<State<Screen>>) {
+    last.and_then(|last| {
+        if last!=*now.get() {
+            info!("state changed to {:?}", *now)
+        }
+        Some(last)
+    });
+    *last= Some(*now.get());
+    
 }
