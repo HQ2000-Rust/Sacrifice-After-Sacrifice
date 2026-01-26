1f18ccd7f700dbf06f30f771000ed541f372c2f6
HQ2000-CPP <194770151+HQ2000-CPP@users.noreply.github.com>
2026-01-26 17:21:30 +0100

    cargo fmt

--- src/ui/title_screen.rs
+++ src/ui/title_screen.rs
@@ -5,7 +5,6 @@ pub fn button(text: impl Into<String>, button_node: Node) -> impl Bundle {
         Button,
         button_node,
         children![(Text::new(text),)],
-        BackgroundColor(Color::rgba(0., 0., 0., 0.7)),
-        
+        BackgroundColor(Color::linear_rgba(0., 0., 0., 0.7)),
     )
 }
