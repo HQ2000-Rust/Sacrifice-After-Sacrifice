5ce643b86f0c7035c5b5a681502e18d6b3a94042
HQ2000-CPP <194770151+HQ2000-CPP@users.noreply.github.com>
2026-01-26 21:58:00 +0100

    feature gated dev plugins

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,21 @@ use bevy::prelude::*;
 use crate::screens::ScreensPlugin;
 
 fn main() -> AppExit {
-    let mut app=App::new();
-        
+    let mut app = App::new();
+
     app.add_plugins((
-            DefaultPlugins,
-            #[cfg(feature = "dev")]
-            FpsOverlayPlugin::default(),
-        ))
-        .add_plugins(ScreensPlugin);
-        
-    #[cfg(feature="ui_debug")]
-    app    
-            .insert_resource(UiDebugOptions {
-                enabled: true,
-                // `UiDebugOptions` has a few new options, but for now we'll leave the defaults.
-                ..default()
-            });
-        
-            app.run()
+        DefaultPlugins,
+        #[cfg(feature = "dev")]
+        FpsOverlayPlugin::default(),
+    ))
+    .add_plugins(ScreensPlugin);
+
+    #[cfg(feature = "ui_debug")]
+    app.insert_resource(UiDebugOptions {
+        enabled: true,
+        // `UiDebugOptions` has a few new options, but for now we'll leave the defaults.
+        ..default()
+    });
+
+    app.run()
 }
