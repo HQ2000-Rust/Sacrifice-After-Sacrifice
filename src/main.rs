585015a31b96af3a6b2cfd01dac88869f73cdc9b
HQ2000-CPP <194770151+HQ2000-CPP@users.noreply.github.com>
2026-01-26 19:36:18 +0100

    add debug overlay feature

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,22 @@ use bevy::prelude::*;
 use crate::screens::ScreensPlugin;
 
 fn main() -> AppExit {
-    App::new()
-        .add_plugins((
+    let mut app=App::new();
+        
+    app.add_plugins((
             DefaultPlugins,
             #[cfg(feature = "dev")]
             FpsOverlayPlugin::default(),
         ))
-        .add_plugins(ScreensPlugin)
-        .run()
+        .add_plugins(ScreensPlugin);
+        
+    #[cfg(feature="ui_debug")]
+    app    
+            .insert_resource(UiDebugOptions {
+                enabled: true,
+                // `UiDebugOptions` has a few new options, but for now we'll leave the defaults.
+                ..default()
+            });
+        
+            app.run()
 }
