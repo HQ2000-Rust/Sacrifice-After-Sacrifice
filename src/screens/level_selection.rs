432bcb23e02758c7c3867ab8a831e054810ba5bc
HQ2000-CPP <194770151+HQ2000-CPP@users.noreply.github.com>
2026-02-04 21:00:12 +0100

    prepare level selection screen

---
+++ src/screens/level_selection.rs
@@ -0,0 +1,25 @@
+use bevy::prelude::*;
+
+use crate::screens::Screen;
+
+struct LevelSelectionPlugin;
+
+impl Plugin for LevelSelectionPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(OnEnter(Screen::LevelSelectionScreen), setup);
+    }
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn((
+        DespawnOnExit(Screen::LevelSelectionScreen),
+        Node {
+            height: percent(100),
+            width: percent(100),
+            ..default()
+        },
+        children![
+            
+        ]
+    ));
+}
