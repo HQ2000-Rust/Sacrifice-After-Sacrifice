87f54b474d34aee05edd006f8951bbda207a960a
aal337 <abdurahmanalgheb@gmail.com>
2026-02-23 00:18:28 +0100

    add plugin for level selection

--- src/screens/mod.rs
+++ src/screens/mod.rs
@@ -1,7 +1,7 @@
+mod level_selection;
 mod splash;
 mod title;
 mod util;
-mod level_selection;
 
 use bevy::prelude::*;
 
@@ -18,7 +18,10 @@ pub struct ScreensPlugin;
 
 impl Plugin for ScreensPlugin {
     fn build(&self, app: &mut bevy::app::App) {
-        app.init_state::<Screen>()
-            .add_plugins((splash::SplashScreenPlugin, title::TitleScreenPlugin));
+        app.init_state::<Screen>().add_plugins((
+            splash::SplashScreenPlugin,
+            title::TitleScreenPlugin,
+            level_selection::LevelSelectionPlugin,
+        ));
     }
 }
