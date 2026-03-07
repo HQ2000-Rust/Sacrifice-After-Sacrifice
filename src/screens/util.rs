f6cca95faee3a648ec3ba223ba500516d0082585
HQ2000-CPP <194770151+HQ2000-CPP@users.noreply.github.com>
2026-03-07 13:49:10 +0100

    added `LevelId` (definitely no overkill)

--- src/screens/util.rs
+++ src/screens/util.rs
@@ -1,3 +1,5 @@
+use std::ops::{RangeBounds, RangeInclusive};
+
 use crate::screens::Screen;
 use bevy::prelude::*;
 
@@ -6,5 +8,39 @@ use bevy::prelude::*;
 pub fn set_screen(next_screen: Screen) -> impl FnMut(ResMut<'_, NextState<Screen>>) + Clone {
     move |mut next_screen_state: ResMut<NextState<Screen>>| {
         next_screen_state.set(next_screen);
+    }
+}
+
+const LEVEL_RANGE: RangeInclusive<u8>= 1..=1;
+
+#[repr(transparent)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord,)]
+pub struct LevelId(u8);
+
+impl LevelId {
+    pub fn new(id: u8) -> Option<LevelId> {
+        if LEVEL_RANGE.contains(&id) {
+            Some(LevelId(id))
+        }
+        else {
+            None
+        }
+    }
+    
+    pub fn get(&self) -> u8 {
+        self.0
+    }
+}
+
+impl TryFrom<u8> for LevelId {
+    type Error=();
+    fn try_from(value: u8) -> std::result::Result<Self, Self::Error> {
+        LevelId::new(value).ok_or(())
+    }
+}
+
+impl Into<u8> for LevelId {
+    fn into(self) -> u8 {
+        self.0
     }
 }
