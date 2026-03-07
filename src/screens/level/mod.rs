0e0ad1fdcc0e4159f2c2b2f32c9d68a09d0fd3e7
HQ2000-CPP <194770151+HQ2000-CPP@users.noreply.github.com>
2026-03-07 14:40:22 +0100

    level prereq

--- src/screens/level/mod.rs
+++ src/screens/level/mod.rs
@@ -1,1 +1,13 @@
 pub mod one;
+
+use bevy::prelude::*;
+
+pub struct LevelScreensPlugin;
+
+impl Plugin for LevelScreensPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_plugins((
+            
+        ))
+    }
+}
